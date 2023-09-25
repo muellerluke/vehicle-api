e28a8858787d614b531e47baa3c78d99eec1227d
muellerluke <66047826+muellerluke@users.noreply.github.com>
2023-09-25 10:46:15 -0500

    added valuation route

--- controllers/valuation_controller.go
+++ controllers/valuation_controller.go
@@ -4,10 +4,17 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+
+	"regexp"
+	"strconv"
+	"strings"
 	"vehicle-api/utils"
 
 	"github.com/goccy/go-json"
 	"github.com/gofiber/fiber/v2"
+
+	"github.com/sajari/regression"
+	"golang.org/x/net/html"
 )
 
 //redis keys
@@ -24,15 +31,15 @@ func ValuationController(c *fiber.Ctx) error {
 	//var zipCode = c.Query("zip_code")
 	//var radius = c.Query("radius")
 
-	response, err := http.Get("https://vpic.nhtsa.dot.gov/api/vehicles/decodevin/" + vin + "?format=json")
+	responseVin, err := http.Get("https://vpic.nhtsa.dot.gov/api/vehicles/decodevin/" + vin + "?format=json")
 	if err != nil {
 		return c.Status(http.StatusInternalServerError).JSON(utils.ApiResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": "Something went wrong. Please try again later."}})
 	}
 
-	defer response.Body.Close()
+	defer responseVin.Body.Close()
 
 	// Read the response body
-	responseBody, err := ioutil.ReadAll(response.Body)
+	responseBody, err := ioutil.ReadAll(responseVin.Body)
 	if err != nil {
 		fmt.Println("Error reading response body:", err)
 		return c.Status(http.StatusInternalServerError).JSON(utils.ApiResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": err.Error()}})
@@ -61,11 +68,197 @@ func ValuationController(c *fiber.Ctx) error {
 	}
 
 	result := decodedVin.Results[0]
-	year := result.Year
-	make := result.Make
-	model := result.Model
-	series := result.Series
+	var year string
+	var make string
+	var model string
+	var series string
 
-	return c.Status(http.StatusOK).JSON(utils.ApiResponse{Status: http.StatusOK, Message: "success", Data: &fiber.Map{"year": year, "make": make, "model": model, "series": series}})
+	for _, result := range decodedVin.Results {
+		if result.Variable == "Model Year" {
+			year = result.Value
+		}
+		if result.Variable == "Make" {
+			make = result.Value
+		}
+		if result.Variable == "Model" {
+			model = result.Value
+		}
+		if result.Variable == "Series" {
+			series = result.Value
+		}
+	}
 
+	if year == "" || make == "" || model == "" || series == "" {
+		fmt.Println("No results found for the VIN.")
+		return c.Status(http.StatusInternalServerError).JSON(utils.ApiResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": "No results found for the VIN."}})
+	}
+
+	//get valuation
+	var url string
+	if radius == "" {
+		url = "https://www.autotrader.com/cars-for-sale/all-cars/" + year + "/" + strings.ReplaceAll(strings.ToLower(make), " ", "-") + "/" + strings.ReplaceAll(strings.ToLower(model), " ", "-") + "?searchRadius=100&zip=" + zipCode
+	} else {
+		url = "https://www.autotrader.com/cars-for-sale/all-cars/" + year + "/" + strings.ReplaceAll(strings.ToLower(make), " ", "-") + "/" + strings.ReplaceAll(strings.ToLower(model), " ", "-") + "?searchRadius=" + radius + "&zip=" + zipCode
+	}
+
+	fmt.Println(url)
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", url, nil)
+
+	response, _ := client.Do(req)
+
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(utils.ApiResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": "Something went wrong. Please try again later."}})
+	}
+	defer response.Body.Close()
+
+	//tokenize the response html
+	z := html.NewTokenizer(response.Body)
+
+	isProductElement := false
+	isPriceElement := false
+	isMileageElement := false
+	isListingTitleElement := false
+	prices := []int{}
+	mileages := []int{}
+	listingTitles := []string{}
+
+	//loop through the tokens
+	for {
+		tt := z.Next()
+
+		switch {
+		case tt == html.ErrorToken:
+			// End of the document, we're done
+
+			//remove all listings that are new vehicles
+			years := []int{}
+
+			for i, listingTitle := range listingTitles {
+				if strings.Contains(listingTitle, "New") {
+					prices = append(prices[:i], prices[i+1:]...)
+					mileages = append(mileages[:i], mileages[i+1:]...)
+				} else {
+					// Define a regular expression pattern to match four consecutive numbers
+					pattern := `[0-9]{4}`
+
+					// Compile the regular expression
+					regexpPattern := regexp.MustCompile(pattern)
+
+					// Find all matches in the input string
+					matches := regexpPattern.FindAllString(listingTitle, -1)
+
+					if len(matches) == 0 {
+						return c.Status(http.StatusInternalServerError).JSON(utils.ApiResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": "Something went wrong. Please try again later."}})
+					}
+
+					year, err := strconv.Atoi(matches[0])
+					if err != nil {
+						fmt.Println("Error converting year to int")
+						return c.Status(http.StatusInternalServerError).JSON(utils.ApiResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": err.Error()}})
+					}
+
+					years = append(years, year)
+				}
+			}
+
+			//check that all maps are the same length
+			if len(prices) != len(mileages) || len(prices) != len(years) {
+				fmt.Println("Error: prices, mileages, and years are not the same length")
+				return c.Status(http.StatusInternalServerError).JSON(utils.ApiResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": "Something went wrong. Please try again later."}})
+			}
+
+			r := new(regression.Regression)
+			r.SetObserved("Price")
+			r.SetVar(0, "Mileage")
+			r.SetVar(1, "Year")
+
+			for i, price := range prices {
+				r.Train(regression.DataPoint(float64(price), []float64{float64(mileages[i]), float64(years[i])}))
+			}
+			r.Run()
+
+			fmt.Printf("Regression formula:\n%v\n", r.Formula)
+			fmt.Printf("Regression:\n%s\n", r)
+
+			//return valuation
+			prediction, err := r.Predict([]float64{float64(mileages[0]), float64(years[0])})
+			if err != nil {
+				fmt.Println("Error predicting price")
+			}
+
+			return c.Status(http.StatusOK).JSON(utils.ApiResponse{Status: http.StatusOK, Message: "success", Data: &fiber.Map{
+				"predicted_price": prediction,
+				"based_on":        strconv.Itoa(len(prices)) + " results",
+				"mileage":         mileages[0],
+				"year":            years[0],
+				"make":            make,
+				"model":           model,
+			}})
+
+		case tt == html.StartTagToken:
+			t := z.Token()
+
+			if t.Type == html.StartTagToken && t.Data == "div" {
+				for _, a := range t.Attr {
+					if a.Key == "class" && strings.Contains(a.Val, "item-card") {
+						isProductElement = true
+					}
+				}
+			}
+
+			if t.Type == html.StartTagToken && t.Data == "span" {
+				for _, a := range t.Attr {
+					if a.Key == "class" && strings.Contains(a.Val, "first-price") {
+						isPriceElement = true
+					}
+					if a.Key == "class" && strings.Contains(a.Val, "text-bold") {
+						isMileageElement = true
+					}
+				}
+			}
+
+			if t.Type == html.StartTagToken && t.Data == "h3" {
+				for _, a := range t.Attr {
+					if a.Key == "class" && strings.Contains(a.Val, "text-bold") {
+						isListingTitleElement = true
+					}
+				}
+			}
+		case tt == html.TextToken:
+			t := z.Token()
+			fmt.Println("isProductElement: ", isProductElement)
+			fmt.Println("isPriceElement: ", isPriceElement)
+
+			if isProductElement {
+				if isMileageElement && strings.Contains(t.Data, " miles") && len(t.Data) < 15 && len(t.Data) > 0 {
+					mileage, err := strconv.Atoi(strings.ReplaceAll(strings.ReplaceAll(t.Data, ",", ""), " miles", ""))
+					if err != nil {
+						fmt.Println("Error converting mileage to int")
+						return c.Status(http.StatusInternalServerError).JSON(utils.ApiResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": err.Error()}})
+					}
+					mileages = append(mileages, mileage)
+					isMileageElement = false
+				}
+
+				if isPriceElement {
+					//log price
+					price, err := strconv.Atoi(strings.ReplaceAll(t.Data, ",", ""))
+					if err != nil {
+						fmt.Println("Error converting price to int")
+						return c.Status(http.StatusInternalServerError).JSON(utils.ApiResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": err.Error()}})
+					}
+					prices = append(prices, price)
+					isProductElement = false
+					isPriceElement = false
+				}
+
+				if isListingTitleElement && len(t.Data) > 0 && len(t.Data) < 250 {
+					listingTitle := t.Data
+					listingTitles = append(listingTitles, listingTitle)
+					isListingTitleElement = false
+				}
+			}
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -31,6 +31,7 @@ require (
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/philhofer/fwd v1.1.2 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/sajari/regression v1.0.1 // indirect
 	github.com/savsgio/dictpool v0.0.0-20221023140959-7bf2e61cea94 // indirect
 	github.com/savsgio/gotils v0.0.0-20230208104028-c358bd845dee // indirect
 	github.com/tinylib/msgp v1.1.8 // indirect
@@ -43,6 +44,9 @@ require (
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	golang.org/x/net v0.12.0 // indirect
 	golang.org/x/sync v0.3.0 // indirect
-	golang.org/x/sys v0.10.0 // indirect
 	golang.org/x/text v0.11.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/tools v0.7.0 // indirect
+	gonum.org/v1/gonum v0.14.0 // indirect
 )
